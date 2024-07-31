b343f8a91463cbbff9e331c1c3ef8234a6305073
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-07-31 09:17:51 +0530

    callback func

--- basic.js
+++ basic.js
@@ -1,3 +1,10 @@
-let a=4;
-a=9;
-console.log(a);
+//callback function
+const callbackfunc=(a,b,ansfunction)=>{
+    console.log(ansfunction(a,b));
+}
+
+const sumfunction=(a,b)=>{
+    let result=a+b;
+    return result;
+}
+ callbackfunc(3,5,sumfunction)
