7aad79cd3ccceef7a328b19a727a0633e00f6315
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-07-31 09:51:50 +0530

    delay between actual time and expected time taken by Timeout func

--- basic.js
+++ basic.js
@@ -1,10 +1,15 @@
-//callback function
-const callbackfunc=(a,b,ansfunction)=>{
-    console.log(ansfunction(a,b));
-}
+const delay = 3000; // 1000 milliseconds (1 second) delay
 
-const sumfunction=(a,b)=>{
-    let result=a+b;
-    return result;
-}
- callbackfunc(3,5,sumfunction)
+// Time when setTimeout is called
+const startTime = Date.now();
+
+
+setTimeout(() => {
+  // Time when the inner function actually runs
+  const endTime = Date.now();
+  const elapsedTime = endTime - startTime;
+
+  console.log(`Expected delay: ${delay} ms`);
+  console.log(`Actual delay: ${elapsedTime} ms`);
+}, delay);
+
