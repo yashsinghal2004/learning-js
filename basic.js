b629ef740f5b8b363fa4a4f5cc3e59f1b0125a48
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-08-01 23:58:09 +0530

    practice

--- basic.js
+++ basic.js
@@ -2,6 +2,3 @@
 let str="hello world"
 let ans=str.slice(0,5);
 
-setInterval(() => {
-    console.log(ans);
-}, 2000);
---
+++ practice.js
@@ -0,0 +1,2 @@
+let str="yash yash singhal";
+console.log(str.lastIndexOf("yash"));
