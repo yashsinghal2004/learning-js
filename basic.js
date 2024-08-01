bd8faf1458cb5fcc5caf1b3affd2ce5e5bd54260
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-08-01 23:50:37 +0530

    practice

--- basic.js
+++ basic.js
@@ -1,15 +1,3 @@
-const delay = 3000; // 1000 milliseconds (1 second) delay
-
-// Time when setTimeout is called
-const startTime = Date.now();
-
-
-setTimeout(() => {
-  // Time when the inner function actually runs
-  const endTime = Date.now();
-  const elapsedTime = endTime - startTime;
-
-  console.log(`Expected delay: ${delay} ms`);
-  console.log(`Actual delay: ${elapsedTime} ms`);
-}, delay);
-
+let str="hello world"
+let ans=str.split(" ");
+console.log(ans);
