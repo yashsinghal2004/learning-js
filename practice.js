0eeadde7cff1b7c30700b165b765211305709016
Yash Singhal <118036819+yashsinghal2004@users.noreply.github.com>
2024-08-02 17:55:25 +0530

    async practice

--- practice.js
+++ practice.js
@@ -1,2 +1,6 @@
-let str="yash yash singhal";
-console.log(str.lastIndexOf("yash"));
+//filesystem
+const fs=require("fs");
+fs.readFile("text.txt", "utf-8", function(err,data){
+    console.log(data);
+});
+console.log("Hi I am yash from after reading file");
